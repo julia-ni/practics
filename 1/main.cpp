0682aa07fc176a8e96d72ea4af14d0e10d5f9547
Julia <43633436+julia-ni@users.noreply.github.com>
2020-03-02 10:56:41 +0000

    >> main, makefile

---
+++ 1/main.cpp
@@ -0,0 +1,90 @@
+#include <cstdio>
+#include <cmath>
+#include <ctime>
+#include <iostream>
+#include <fstream>
+#include <complex>
+#include <omp.h>
+
+using namespace std;
+
+typedef complex<double> complexd;
+
+void
+one_qubit_conversion(complexd *A, complexd *B, double H[2][2], int n, int k){
+	long long int shift_n, shift_nk, inv_nk, change_bit = 0;
+	shift_n = 1 << n;
+	shift_nk = 1 << (n - k);
+	inv_nk = ~shift_nk;
+#pragma omp parallel for firstprivate(change_bit)
+	for (long long int i = 0; i < shift_n; i++) {
+		change_bit = (i & shift_nk) >> (n - k);
+		B[i] = A[i & inv_nk] * H[change_bit][0] + A[i | shift_nk] * H[change_bit][1];
+	}
+}
+
+int main(int argc, char **argv) {
+	enum {ARG_CNT = 4};
+	if (argc != ARG_CNT) {
+		cerr << "Invalid call. Please type n, k, thr_num." << endl;
+		exit(1);
+	}
+	int n, k, thr_num;
+	n = atoi(argv[1]);
+	k = atoi(argv[2]);
+	if (k > n) {
+		cerr << "Invalid values." << endl;
+		exit(1);
+	}
+	thr_num = atoi(argv[3]);
+	omp_set_num_threads(thr_num);
+
+	// A initialization
+	long long int shift_n = 1 << n; // 2^n
+	complexd *A = new complexd[shift_n];
+	srand(omp_get_wtime());
+	int real_part = 0, imag_part = 0;
+	float half_max = RAND_MAX / 2;
+	float sum = 0;
+#pragma omp parallel for firstprivate(real_part, imag_part) reduction(+: sum)
+	for (long long int i = 0; i < shift_n; i++) {
+		real_part = rand();
+		if (real_part < half_max) {
+			real_part *= -1;
+		}
+		real_part *= static_cast <float> (rand() / static_cast <float> (RAND_MAX));
+		imag_part = rand();
+		if (imag_part < half_max) {
+			imag_part *= -1;
+		}
+		imag_part *= static_cast <float> (rand() / static_cast <float> (RAND_MAX));
+		A[i].real(real_part);
+		A[i].imag(imag_part);
+		sum += abs(A[i] * A[i]);
+	}
+	sum = sqrt(sum);
+#pragma omp parallel for
+	for (long long int i = 0; i < shift_n; i++) {
+		A[i] /= sum;
+	}
+
+	// Conversion
+	double tmp_sqrt = 1 / sqrt(2);
+	double H[2][2] = {tmp_sqrt, tmp_sqrt, tmp_sqrt, (-1) * tmp_sqrt};
+	complexd *B = new complexd[shift_n];
+
+	double begin, end, comp_time;
+	begin = omp_get_wtime();
+	one_qubit_conversion(A, B, H, n, k);
+	end = omp_get_wtime();
+	comp_time = end - begin;
+
+	ofstream filename;
+	filename.open("a.txt", ofstream::out | ofstream::app);
+	filename << "n: " << n << " k: " << k << " thr_num: " << thr_num << " time: " << comp_time << endl;
+	filename.close();
+
+	delete [] A;
+	delete [] B;	
+	return 0;
+}
